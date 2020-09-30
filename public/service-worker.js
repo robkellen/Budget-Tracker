57acb254fadae52732965953f400748ec6bea7cb
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-29 23:09:54 -0700

    amended service-worker.js file.  Was not previously able to get cached data from deployed app

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,32 +6,29 @@ const FILES_TO_CACHE = [
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
   "manifest.webmanifest",
-  "db.js"
+  "db.js",
 ];
 
-const CACHE_NAME = "static-cache-v2";
+const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-self.addEventListener("install", function (evt) {
- // pre cache all static assets
+self.addEventListener("install", function(evt) {
   evt.waitUntil(
+    //pre-cache all static files
     caches.open(CACHE_NAME).then(cache => {
       return cache.addAll(FILES_TO_CACHE);
     })
-
-  // console.log("Up and running!");
   );
-  // tell the browser to activate this service worker immediately once it
-  // has finished installing
+
   self.skipWaiting();
 });
 
 // activate
-self.addEventListener("activate", function (evt) {
+self.addEventListener("activate", function(evt) {
   evt.waitUntil(
-    caches.keys().then((keyList) => {
+    caches.keys().then(keyList => {
       return Promise.all(
-        keyList.map((key) => {
+        keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
             console.log("Removing old cache data", key);
             return caches.delete(key);
@@ -46,7 +43,7 @@ self.addEventListener("activate", function (evt) {
 
 // fetch
 self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
+  if (evt.request.url.includes("/api/") && evt.request.method === "GET") {
     evt.respondWith(
       caches
         .open(DATA_CACHE_NAME)
