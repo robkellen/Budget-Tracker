33447d258232f2da7087a0341fb69c733452bc5b
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-29 20:45:11 -0700

    deploying

--- server.js
+++ server.js
@@ -17,12 +17,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
-
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
